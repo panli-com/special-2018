d2592d618104dfdf55a7f7522389e31098d30a91
zanjs <root@zanjs.com>
2018-02-22 13:32:05 +0800

    update

--- 0211/data/app.js
+++ 0211/data/app.js
@@ -8,48 +8,43 @@ function FloorList(data) {
     fl6 = [],
     fl0 = [];
 
-  for (var i = 0; i < mlc; i++) {
+  for (var i = 0; i < mlc + 4; i++) {
     fl0.push(data[i]);
   }
-  for (var i = mlc; i < mlc * 2; i++) {
+  for (var i = mlc + 4; i < mlc * 2 + 4; i++) {
     fl1.push(data[i]);
   }
-  for (var i = mlc * 2; i < mlc * 3; i++) {
+  for (var i = mlc * 2 + 4; i < mlc * 3 + 4; i++) {
     fl2.push(data[i]);
   }
-  for (var i = mlc * 3; i < mlc * 4; i++) {
+  for (var i = mlc * 3 + 4; i < mlc * 4 + 4; i++) {
     fl3.push(data[i]);
   }
-  for (var i = mlc * 4; i < mlc * 5; i++) {
+  for (var i = mlc * 4 + 4; i < mlc * 5 + 4; i++) {
     fl4.push(data[i]);
   }
-  for (var i = mlc * 5; i < mlc * 6; i++) {
+  for (var i = mlc * 5 + 4; i < mlc * 6 + 4; i++) {
     fl5.push(data[i]);
   }
   // for (var i = mlc * 6; i < mlc * 7; i++) {
   //     fl6.push(data[i]);
   // }
 
-
-
-  FloorAll(fl0, 'cx', 1);
-  FloorAll(fl1, 'ts', 2);
-  FloorAll(fl2, 'rw', 3);
-  FloorAll(fl3, 'sh', 4);
-  FloorAll(fl4, 'sh', 5);
+  FloorAll(fl0, "cx", 1);
+  FloorAll(fl1, "ts", 2);
+  FloorAll(fl2, "rw", 3);
+  FloorAll(fl3, "sh", 4);
+  FloorAll(fl4, "sh", 5);
   // FloorAll(fl5, 'sh', 6);
   // FloorAll(fl6, 'sh', 7);
-
-
 }
 
 function FloorAll(data, imgHead, id) {
-
-  var str = '';
+  var str = "";
   for (var i = 0; i < data.length; i++) {
     var name = data[i].店铺名称,
       price = data[i].价格,
-      imgUrl = './build/css/img/products/' + data[i].图片名称,
+      imgUrl = "./build/css/img/products/" + data[i].图片名称,
       Url = "http://www.panli.com/Crawler.aspx?purl=" + data[i].商品导购,
       shopUrl = data[i].店铺链接;
 
@@ -74,9 +69,9 @@ function FloorAll(data, imgHead, id) {
                         <span>进店疯抢</span>
                         </a>
                         </div>
-                </li>`
-
-
-  };
-  $("#floor-" + id).find("ul").html(str);
+                </li>`;
+  }
+  $("#floor-" + id)
+    .find("ul")
+    .html(str);
 }
